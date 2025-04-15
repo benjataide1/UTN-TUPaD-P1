5c8d32445588c47dea826bea3c14a3a50767aef5
Benja <benjaminatai@gmail.com>
2025-04-14 21:42:16 -0300

    Estructuras TP - 4

--- 04 Estructuras Repetitivas/EscturctuaRepetitiva_TP.py
+++ 04 Estructuras Repetitivas/EscturctuaRepetitiva_TP.py
@@ -3,19 +3,18 @@ from random import randint as ran
 
 # Actividad 1
 
-"""
-for i in range(0,101,1):
-  print(i)
-"""
-# Activadad 2
-"""
+
+for i in range(0, 101, 1):
+    print(i)
+
+
 num = input("Ingrese un numero para ver la cantidad de digitios: ")
 print(len(num))
-"""
+
 
 # Actividad 3
 
-"""
+
 inicio = int(input("Ingrese un Numeo para iniciar la iteracion: "))
 final = int(input("Ingrese un Numero hasta donde quiere iterar: "))
 
@@ -25,10 +24,10 @@ for i in range(inicio + 1, final):
     count += i
 
 print(f"El total es:{count}")
-"""
+
 
 # Actividadd 4
-"""
+
 count = 0
 condicion = True
 while condicion:
@@ -38,10 +37,10 @@ while condicion:
     elif (num == 0):
         print(f"El total acomulado es:{count}")
         condicion = False
-"""
+
 
 # Actividad 5
-"""
+
 aleatorio = ran(0, 9)
 intentos = 0
 num = 0
@@ -54,27 +53,27 @@ while aleatorio:
     intentos += 1
 
 print(f"Total de intentos {intentos}- El numero es:{num}")
-"""
+
 
 # Actividad 6
-"""
+
 for i in range(100, -1, -1):
     if (i % 2 == 0):
         print(f"Los numeros pares son {i}")
-"""
+
 
 # Actividad 7
-"""
+
 num = int(input("Ingrese un numero para indicar el final de la iteracion: "))
 count = 0
-for i in range(0,num):
-  count +=i
+for i in range(0, num):
+    count += i
 print(f"La suma de los numeros es:{count}")
-"""
+
 
 # Actividad 8
 
-"""
+
 pares = 0
 impares = 0
 negativos = 0
@@ -96,11 +95,11 @@ while contador < 100:
 
 print(
     f"Pares:{pares}, Impares:{impares},Negativos:{negativos},Positivos:{positivos}")
-"""
+
 
 # Actividad 9
 
-"""
+
 total = 0
 contador = 0
 while contador < 10:
@@ -110,7 +109,7 @@ while contador < 10:
     contador += 1
 
 print(f"La Media es {total / contador}")
-"""
+
 
 # Actividad 10
 
@@ -123,4 +122,3 @@ while (count > 0):
     count -= 1
 
 print(f"El numero reverso es: {numReverso}")
-
